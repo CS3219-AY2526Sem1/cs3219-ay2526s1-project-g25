2c97184df3f6552681376f1d1bb0d857a621f414
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2025-09-30 01:36:00 +0800

    Fix Jest setup bug

--- collaboration-service/jest.config.cjs
+++ collaboration-service/jest.config.cjs
@@ -1,1 +1,9 @@
-export default { testEnvironment: 'node', roots: ['<rootDir>/src/__tests__'] };
+// export default { testEnvironment: 'node', roots: ['<rootDir>/src/__tests__'] };
+// module.exports = { testEnvironment: 'node', roots: ['<rootDir>/src/__tests__'] };
+module.exports = {
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src/__tests__'],
+    // No extensionsToTreatAsEsm needed since "type":"module" is set
+    transform: {}, // keep empty to avoid Babel
+};
+
