006ec123142edb3063a86bb8313fa03845b0b9cc
Evelyne Law <lawruixi@gmail.com>
2025-10-02 09:36:49 +0800

    [WIP] Added REST API part of Integration Test.

--- collaboration-service/src/__tests__/integration/app.test.js
+++ collaboration-service/src/__tests__/integration/app.test.js
@@ -25,3 +25,46 @@ describe('sessions REST', () => {
     expect(get.body.presence.length).toBe(1);
   });
 });
+
+describe("REST and Websocket", () => {
+    let server;
+    let baseURL;
+
+    beforeAll((done) => {
+        server = createServer(app);
+        server.listen(0, () => {
+            const { port } = server.address();
+            baseURL = `http://localhost:${port}`;
+            done();
+        })
+    })
+
+    afterAll((done) => {
+        server.close(done);
+    })
+
+    test("pairs two users and syncs updates", async () => {
+        const sessionCreate = await request(baseURL)
+            .post('/sessions')
+            .send({ userA: 'alice', userB: 'bob', topic: 'arrays', difficulty: 'hard', questionId: '123' })
+            .expect(201);
+        const sessionId = sessionCreate.body.id;
+        expect(sessionId).toBeDefined();
+
+        const sessionExists = await request(baseURL)
+            .get(`/sessions/${sessionId}`)
+            .send({ id: sessionId })
+            .expect(200);
+        expect(sessionExists.body).toBeDefined();
+
+        const userA = await request(baseURL)
+            .post(`/sessions/${sessionId}/join`)
+            .send({ userId: 'alice' })
+            .expect(200);
+
+        const userB = await request(baseURL)
+            .post(`/sessions/${sessionId}/join`)
+            .send({ userId: 'bob' })
+            .expect(200);
+    })
+})
