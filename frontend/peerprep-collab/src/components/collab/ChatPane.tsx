01a880259b78e7dc65a52a38af06908e6807f549
Evelyne Law <lawruixi@gmail.com>
2025-10-16 09:52:05 +0800

    Made ChatBox stay at bottom.

--- frontend/peerprep-collab/src/components/collab/ChatPane.tsx
+++ frontend/peerprep-collab/src/components/collab/ChatPane.tsx
@@ -3,8 +3,8 @@ import ChatBubble from "./ChatBubble";
 
 export default function ChatPane() {
     return (
-        <div className="w-1/3 h-full bg-white border border-gray-200 rounded-2xl p-6 shadow-sm overflow-y-scroll">
-            <div className="w-full h-max flex flex-col gap-2.5 pb-10">
+        <div className="w-1/3 h-full bg-white border border-gray-200 rounded-2xl p-6 shadow-sm flex flex-col">
+            <div className="w-full h-max flex flex-col gap-2.5 pb-10 flex-1 overflow-y-auto">
                 <ChatBubble isUser={false}>TEST MESSAGE</ChatBubble>
                 <ChatBubble isUser={false}>TEST MESSAGE</ChatBubble>
                 <ChatBubble isUser={true}>TEST MESSAGE</ChatBubble>
@@ -26,7 +26,7 @@ export default function ChatPane() {
                 <ChatBubble isUser={true}>TEST MESSAGE</ChatBubble>
                 <ChatBubble isUser={false}>TEST MESSAGE</ChatBubble>
             </div>
-            <div className="w-full">
+            <div className="mt-4 sticky bottom-0 bg-white pt-2 border-t border-gray-100">
                 <ChatBox/>
             </div>
         </div>
