4b9a40b59c92a8d03ff41fe2a603fe7f595d1eb7
Evelyne Law <lawruixi@gmail.com>
2025-10-17 14:50:54 +0800

    Added Websocket functionality to Question Pane!!

--- frontend/peerprep-collab/src/app/collab/page.tsx
+++ frontend/peerprep-collab/src/app/collab/page.tsx
@@ -1,12 +1,80 @@
+"use client";
+
 import ChatPane from "@/components/collab/ChatPane";
 import CodePane from "@/components/collab/CodePane";
 import ExecutionPane from "@/components/collab/ExecutionPane";
 import QuestionPane from "@/components/collab/QuestionPane";
+import {useEffect, useState} from "react";
 
 export default function CollabPage() {
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+    const [sessionId, setSessionId] = useState<string>("");
+    const [userId, setUserId] = useState<string>("");
+    const [messages, setMessages] = useState<string[]>([]);
+    const [connected, setConnected] = useState(false);
+
+    // TEMPORARY: Question Data.
+    const question = {
+        title: "Find Maximum Element",
+        description: "Find the largest element given an array.",
+        difficulty: "easy",
+        topic: "Graphs",
+        test_cases: [
+            {
+                "input": "[1, 9, 3]",
+                "output": "9"
+            },
+            {
+                "input": "[5, 2, 8]",
+                "output": "8"
+            }
+        ],
+        imageUrl: "https://picsum.photos/200"
+    }
+
+    // TEMPORARY: Used for getting the details needed to connect to WebSocket, which Matching Service should provide.
+    useEffect(() => {
+        // Ask for temporary IDs via dialog boxes.
+        const sId = prompt("Enter session ID:") || "";
+        const uId = prompt("Enter user ID:") || "";
+        setSessionId(sId);
+        setUserId(uId);
+
+        if (!sId || !uId) { return; }
+
+        const wsUrl = `ws://localhost:3004/ws?sessionId=${encodeURIComponent(sId)}&userId=${encodeURIComponent(uId)}`;
+            console.log(wsUrl);
+        const ws = new WebSocket(wsUrl);
+
+        ws.onopen = () => {
+            console.log(`Connected to websocket server at ${wsUrl}`);
+            setConnected(true);
+        }
+
+        ws.onmessage = (event) => {
+            setMessages((prevMessages) => {
+                return [...prevMessages, event.data]
+            })
+        }
+
+        ws.onclose = () => {
+            console.log("Disconnected.");
+            setConnected(false);
+        }
+
+        ws.onerror = (err) => {
+            console.error("Websocket Error:", err);
+        }
+        
+        setSocket(ws);
+
+        // Cleanup on unmount
+        return () => ws.close();
+    }, []);
+
     return (
         <div className="h-screen flex flex-row">
-            <QuestionPane/>
+            <QuestionPane question={question}/>
             <div className="w-1/3 flex flex-col">
                 <CodePane/>
                 <ExecutionPane/>
--- frontend/peerprep-collab/src/components/collab/QuestionPane.tsx
+++ frontend/peerprep-collab/src/components/collab/QuestionPane.tsx
@@ -1,35 +1,19 @@
 import TagPill from "./TagPill"
 import TestCase from "./TestCase"
 
-export default function QuestionPane() {
-    const test_cases = {
-        "cases": [
-            {
-                "input": "[1, 9, 3]",
-                "output": "9"
-            },
-            {
-                "input": "[5, 2, 8]",
-                "output": "8"
-            }
-        ]
-    }
-
-    const question_title = "Find Maximum Element";
-    const question_description = "Find the largest element given an array.";
-    const difficulty = "easy";
-    const topic = "Graphs";
+export default function QuestionPane({question}) {
+    const {title, description, difficulty, topic, test_cases, imageUrl} = question;
 
     return (
         <div className="w-1/3 h-full bg-white border border-gray-200 rounded-2xl p-6 shadow-sm overflow-y-scroll">
-            <h2 className="text-2xl font-bold text-gray-900 mb-2">{question_title}</h2>
+            <h2 className="text-2xl font-bold text-gray-900 mb-2">{title}</h2>
             <div className="flex flex-wrap mb-4">
                 <TagPill label={difficulty} isDifficulty={true}/>
                 <TagPill label={topic} isDifficulty={false}/>
             </div>
 
-            <p className="text-gray-700 leading-relaxed mb-4">{question_description}</p>
-            <img className="rounded-lg mb-5 w-full object-cover" alt="Question Image" src="https://picsum.photos/200"></img>
+            <p className="text-gray-700 leading-relaxed mb-4">{description}</p>
+            <img className="rounded-lg mb-5 w-full object-cover" alt="Question Image" src={imageUrl}></img>
 
             <h3 className="text-lg font-semibold text-gray-800 mb-3">Test Cases</h3>
             { (test_cases["cases"] || []).map((el) => 
