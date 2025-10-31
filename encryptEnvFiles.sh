305276beee78fe5884a15c0c5e847a009f4a361a
Evelyne Law <lawruixi@gmail.com>
2025-10-31 15:20:18 +0800

    Added Matching UI deployment to workflow.

--- encryptEnvFiles.sh
+++ encryptEnvFiles.sh
@@ -3,3 +3,4 @@ gpg --batch --yes --passphrase-file .gpg.passphrase -c matching-service/matching
 gpg --batch --yes --passphrase-file .gpg.passphrase -c user-service/user-service/.env
 gpg --batch --yes --passphrase-file .gpg.passphrase -c question-service/question-service/.env
 gpg --batch --yes --passphrase-file .gpg.passphrase -c feature-login-signup-ui/frontend/.env
+gpg --batch --yes --passphrase-file .gpg.passphrase -c feature-matching-ui/frontend/.env
